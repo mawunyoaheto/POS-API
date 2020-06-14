21f450f9b1217371c694119874f9d0b6d9b1f8e7
mawunyo <raymondaheto@gmail.com>
2020-06-14 18:09:21 +0000

    cleaned up code

--- controllers/products.js
+++ controllers/products.js
@@ -3,31 +3,15 @@ var moment = require('moment');
 var uuidv4 = require('uuidv4');
 const db = require('../util/db_worm');
 const helper = require('../util/helper');
+var dbConfig = require('../config');
+
+const userid = `${dbConfig.app_user}`;
+const userMachineName = `${dbConfig.userMachine}`;
+const userMachineIP = `${dbConfig.userIP}`;
 
 
-
-
-/**
- * @swagger
- * tags:
- *   name: Products
- *   description: Products management
- */
-
-
-/**
- * @swagger
- * /product-category:
- *  get:
- *    summary: Get all product categories
- *    tags: [Products]
- *    description: Used to get all product categories
- *    responses:
- *      '200':
- *        description: A succesful response
- */
-  
-async function getProductCategories(req, res) {
+//GET ALL PRODUCT CATEGORIES
+async function getProductCategories(req, res,error) {
   
   const pool = await db.dbConnection()
 
@@ -35,50 +19,24 @@ async function getProductCategories(req, res) {
 
     pool.query('select * from product_categories', function (err, recordset) {
 
-      if (err) {
-
-        console.log(err)
+      if (recordset.rowCount > 0) {
+        // send records as a response
+        return res.status(200).json(recordset.rows)
 
       } else {
-
-        // send records as a response
-        res.status(200).json(recordset.rows);
+        return res.status(404).json({ 'message': 'failed with no records found' })
       }
-    });
+  });
 
-
-  } catch (error) {
-
-    console.log(error)
-
-  }
+} catch (error) {
+  return res.status(400).json('record not found with error: ' + helper.parseError(error, queryString))
+}
 
 }
 
 
-
-/**
- * @swagger
- * path:
- *  /product-category/catID:
- *    get:
- *      summary: Get a product category by id
- *      tags: [Products]
- *      parameters:
- *          name: catID
- *          -in: path
- *          description: id of product category to fetch
- *          schema:
- *            type: string
- *          required: true
- *      responses:
- *        '200':
- *          description: A succesful response
- *          content:
- *            application/json:
- */
-
-async function getProductCategoryID(catID, res) {
+//GET PRODUCT CATEGORY BY ID
+async function getProductCategoryID(catID, res, error) {
 
   const pool = await db.dbConnection()
 
@@ -86,163 +44,102 @@ async function getProductCategoryID(catID, res) {
 
     const recordset = await pool.query(`select * FROM product_categories WHERE id='${catID}'`)
 
-    if (recordset.rowsAffected > 0) {
-      return res.status(200).json( recordset.recordset[0].category)
-      //console.log('userCategory-id', recordset.recordset[0].id)
-    } else {
-      return {
-        message: 'No record found'
-      }
+    if (recordset.rowCount > 0) {
+      // send records as a response
+      return res.status(200).json(recordset.rows)
 
+    } else {
+      return res.status(404).json({ 'message': 'failed' })
     }
 
-  } catch (error) {
-    console.log(error);
-    res.end()
 
-  }
+} catch (error) {
+return res.status(400).json('record not found with error: ' + helper.parseError(error, queryString))
+}
 
 }
 
 
-/**
- * @swagger
- * /product-category:
- *  post:
- *    summary: Add a new product category
- *    tags: [Products]
- *    description: Used to create a product category
- *    responses:
- *      '200':
- *        description: A succesful response
- */
 
-async function createPoductCategory(req, res,err) {
+//CREATE PRODUCT CATEGORY
+async function createPoductCategory(req, res, error) {
 
     const pool = await db.dbConnection()
   
-    const createQuery = `INSERT INTO product_categories(category, create_date) VALUES($1, $2) returning *`;
-
-    const createCategoryLineQuery = `INSERT INTO public.product_categories_lines (categoryid, description, qty)
-          VALUES ($1, $2, $3) returning *`;
+    const createQuery = `INSERT INTO public.product_categories(category, isactive, create_userid, create_date, usermachinename, usermachineip) 
+    VALUES ( $1, $2, $3, $4, $5, $6); returning *`;
 
     const values = [
-      req.body[0].category,
-      moment(new Date())
+      req.body.category,
+      req.body.isactive,
+      req.body.userId,
+      moment(new Date()),
+      userMachineName,
+      userMachineIP
     ];
   
       try {
-        console.log('inside-inner try','I am here')
-        await pool.query('BEGIN')
+      
+          const row_count = await pool.query(createQuery, values);
 
-        try {
+             if (row_count.rowCount > 0) {
+          // send records as a response
+          return res.status(201).json({ 'message': 'success' })
 
-          console.log('inside-inner try','I am here')
-          const { rows } = await pool.query(createQuery, values);
-          
-          
-          var catID=rows[0].id
-          console.log('Return-ID',catID)
-
-          for(var i=0; i<req.body.length; i++) {
-   
-            var orderLinesvalues = [
-                catID,
-                req.body[i].description,
-                req.body[i].qty
-            ];  
-
-            //console.log(i,req.body[i].description + '' +req.body[i].qty)
-            
-            var { rowslines } = await pool.query(createCategoryLineQuery, orderLinesvalues);
+        } else {
+          return res.status(402).json({ 'message': 'failed' })
         }
-    
-          console.log('out-of-loop','Success')
-          pool.query('COMMIT')
-          return res.status(201).send({ 'message': 'created succesfully' });
- 
-        } catch (error) {
-          console.log(error)
-          console.log('inside-inner-catch','I am here')
-          pool.query('ROLLBACK')
 
-          res.status(400).send(error);
-        }
-  
-    } catch (error) {
-      console.log('inside-outer-catch','I am here')
-      res.status(400).send(error);
-      console.log(error)
-      pool.query('ROLLBACK')
-    }
-  
+  } catch (error) {
+    return res.status(400).json('record insert failed with error: ' + helper.parseError(error, createQuery))
+  }
   }
 
 
-/**
- * @swagger
- * /product-category:
- *  put:
- *    summary: update a product category by ID
- *    tags: [Products]
- *    description: Used to update a product category
- *    responses:
- *      '200':
- *        description: A succesful response
- */
 
-  async function updateProductCategory(req, res) {
+//UPDATE PRODUCT CATEGORY
+  async function updateProductCategory(req, res, error) {
 
     const id = req.params.id;
     const pool = await db.dbConnection();
   
     const values = [
       req.body.category,
-      id
+      req.body.isactive,
+      req.body.userId,
+      moment(new Date()),
+      userMachineName,
+      userMachineIP
     ];
-    const findonequery = 'SELECT * FROM product_categories WHERE id = ($1)';
-    const updateonequery = `UPDATE product_categories SET category = '${req.body.category}', modified_date='${moment(new Date())}' WHERE id = '${id}' returning *`;
+   
+    const updateonequery = `UPDATE public.product_categories SET category='${req.body.category}', isactive='${req.body.isactive}', 
+    modified_date='${moment(new Date())}', modifier_userid='${req.body.userId}', usermachinename='${userMachineName}', 
+    usermachineip='${userMachineIP}' WHERE id = '${id}' returning *`;
   
-   //const confirmed = await confirmRecord(findonequery, id);
     const confirmed = await helper.confirmRecord(findonequery, id);
-    if (confirmed) {
   
       try {
         //update is done here
-        await pool.query(updateonequery, (err, res, next) =>{
-  
-          console.log(res.rows)
-         
-        });
+      const row_count =  await pool.query(updateonequery)
+
+      if (row_count.rowCount > 0) {
         
-  res.status(201).json({ 'message': 'Product Category updated succesfully' });
-      } catch (error) {
-        console.log('UPDATE-ERROR',error)
-        res.status(404).json({ 'error': error });
+        res.status(201).json({ 'message': 'success' });
+      } else {
+        res.status(402).json({ 'message': 'updated' });
       }
+        
   
-      //res.status(200).json({ 'message': 'record found' })
+      } catch (error) {
   
-    } else {
-  
-      res.status(404).json({ 'message': 'Update failed: record does not exist' });
+        return res.status(400).json('record not found with error: ' + helper.parseError(error, queryString))
     }
-  
-  };
-  
+
+  }
 
   //GET Products
-
   
-  /**
-   * get all Products
-   * @param {object} req 
-   * @param {object} res
-   * @returns {object} user category object 
-   */
-  
-  
-  async function getProducts(req, res) {
+  async function getProducts(req, res, error) {
   
     const pool = await db.dbConnection()
   
@@ -250,38 +147,25 @@ async function createPoductCategory(req, res,err) {
   
       pool.query('select * from products', function (err, recordset) {
   
-        if (err) {
-  
-          console.log(err)
+        if (recordset.rowCount > 0) {
+          // send records as a response
+          return res.status(200).json(recordset.rows)
   
         } else {
-  
-          // send records as a response
-          res.status(200).json(recordset.rows);
+          return res.status(404).json({ 'message': 'No Record Found' })
         }
-      });
+    });
   
-  
-    } catch (error) {
-  
-      console.log(error)
-  
-    }
+  } catch (error) {
+    return res.status(400).json('record not found with error: ' + helper.parseError(error, queryString))
+  }
   
   }
 
 
   //GET Products BY ID
 
-  /**
- * get Product by ID
- * @param {object} req 
- * @param {object} res
- * @returns {object} user category object 
- */
-
-
-async function getProductByID(catID, res) {
+async function getProductByID(catID, res, error) {
 
   const pool = await db.dbConnection()
 
@@ -289,21 +173,18 @@ async function getProductByID(catID, res) {
 
     const recordset = await pool.query(`select * FROM products WHERE id='${catID}'`)
 
-    if (recordset.rowsAffected > 0) {
-      return recordset.recordset[0].category
-      //console.log('userCategory-id', recordset.recordset[0].id)
-    } else {
-      return {
-        message: 'No record found'
-      }
+    if (recordset.rowCount > 0) {
+      // send records as a response
+      return res.status(200).json(recordset.rows)
 
+    } else {
+      return res.status(404).json({ 'message': 'failed' })
     }
 
-  } catch (error) {
-    console.log(error);
-    res.end()
 
-  }
+} catch (error) {
+return res.status(400).json('record not found with error: ' + helper.parseError(error, queryString))
+}
 
 }
 
@@ -313,10 +194,10 @@ async function getProductByID(catID, res) {
 
     const pool = await db.dbConnection()
   
-    const createQuery = `INSERT INTO products(
-      description, extended_description, product_code, cost_price, s_price, category_id, create_user_id)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
-        returning *`;
+    const createQuery = `INSERT INTO public.products(description, extended_description, product_code, cost_price, s_price, category_id, 
+      create_userid, create_date, archived, usermachinename, usermachineip)
+      VALUES ($1, $2, $, $4, $5, $6, $7, $8, $9, $10, $11) returning *`;
+
     const values = [
       req.body.description,
       req.body.ext_description,
@@ -324,72 +205,73 @@ async function getProductByID(catID, res) {
       req.body.cost_price,
       req.body.s_price,
       req.body.category_id,
-      req.body.create_user_id
+      req.body.create_user_id,
+      moment(new Date()),
+      req.body.archived,
+      userMachineName,
+      userMachineIP
     ];
   
       try {
-      const { rows } = await pool.query(createQuery, values);
+      const row_count = await pool.query(createQuery, values);
   
-      return res.status(201).send({ 'message': 'Product created succesfully' });
-  
-    } catch (error) {
-  
-      res.status(400).send(error);
-    }
+      if (row_count.rowCount > 0) {
+        // send records as a response
+        return res.status(201).json({ 'message': 'success' })
+
+      } else {
+        return res.status(402).json({ 'message': 'failed' })
+      }
+
+} catch (error) {
+  return res.status(400).json('record insert failed with error: ' + helper.parseError(error, createQuery))
+}
   }
 
+
 //update a Product
-
-
-async function updateProduct(req, res) {
+async function updateProduct(req, res, error) {
 
   const id = req.params.id;
   const pool = await db.dbConnection();
 
   const values = [
-    req.body.description,
-    req.body.ext_description,
-    req.body.product_code,
-    req.body.cost_price,
-    req.body.s_price,
-    req.body.category_id,
-    req.body.archived,
-    moment(new Date()),
-    req.body.modifier_userid,
-    id
+   req.body.description,
+      req.body.ext_description,
+      req.body.product_code,
+      req.body.cost_price,
+      req.body.s_price,
+      req.body.category_id,
+      req.body.userid,
+      moment(new Date()),
+      req.body.archived,
+      userMachineName,
+      userMachineIP
   ];
-  const findonequery = 'SELECT * FROM products WHERE id = ($1)';
-  //const updateonequery = `UPDATE products SET category = '${req.body.category}', modified_date='${moment(new Date())}' WHERE id = '${id}' returning *`;
-  const updateonequery = `UPDATE products
-  SET description='${req.body.description}', extended_description='${req.body.ext_description}', product_code='${req.body.product_code}', 
-  cost_price='${req.body.cost_price}', s_price='${req.body.s_price}', category_id='${req.body.category_id}', 
-  archived='${req.body.archived}', modified_date='${moment(new Date())}', modifier_id='${req.body.modifier_userid}' WHERE id = '${id}' returning *`;
+ 
+  const updateonequery = `UPDATE products SET description='${req.body.description}', extended_description='${req.body.ext_description}', 
+  product_code='${req.body.product_code}', cost_price='${req.body.cost_price}', s_price='${req.body.s_price}', category_id='${req.body.category_id}', 
+  archived='${req.body.archived}', modified_date='${moment(new Date())}', modifier_id='${req.body.userid}', usermachinename='${userMachineName}', 
+  usermachineip='${userMachineIP}' WHERE id = '${id}' returning *`;
 
-  const confirmed = await confirmRecord(findonequery, id);
-  if (confirmed) {
 
     try {
-      //update is done here
-      await pool.query(updateonequery, (err, res, next) =>{
+    
+     const row_count = await pool.query(updateonequery)
 
-        //console.log(res.rows)
-       
-      });
-      
-    res.status(201).json({ 'message': 'Product updated succesfully' });
-    } catch (error) {
-      console.log('UPDATE-ERROR',error)
-      res.status(404).json({ 'error': error });
-    }
+        if (row_count.rowCount > 0) {
+          // send records as a response
+          return res.status(201).json({ 'message': 'success' })
 
-    //res.status(200).json({ 'message': 'record found' })
+        } else {
+          return res.status(402).json({ 'message': 'failed' })
+        }
 
-  } else {
+  } catch (error) {
+    return res.status(400).json('record update failed with error: ' + helper.parseError(error, updateonequery))
 
-    res.status(404).json({ 'message': 'Update failed: record does not exist' });
   }
-
-};
+}
   module.exports={
       createPoductCategory,
       getProductCategories,
